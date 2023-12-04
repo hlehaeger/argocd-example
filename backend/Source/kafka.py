e131ce57cd9dc10f3e190e8cbdc6d83cae2101e1
Hieu Le <lehieu.jo@gmail.com>
2023-12-04 16:36:26 +0100

    add backend

---
+++ backend/Source/kafka.py
@@ -0,0 +1,18 @@
+from kafka import KafkaProducer, KafkaConsumer
+import json
+
+bootstrap_servers = 'kafka.default.svc.cluster.local:9092'
+# Set up Kafka producer
+producer = KafkaProducer(
+    bootstrap_servers=bootstrap_servers,
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
+
+# # Set up Kafka consumer
+# consumer = KafkaConsumer(
+#     'your_kafka_topic',
+#     bootstrap_servers='your_kafka_bootstrap_servers',
+#     group_id='your_consumer_group',
+#     auto_offset_reset='earliest',
+#     value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+# )
